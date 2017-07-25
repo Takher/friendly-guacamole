b6a7b3695d81bcb400947f20c5e31356eb249d27
Tanrajbir <tanrajbir@gmail.com>
2017-07-25 12:15:19 +0100

    refactor and remove stopwords

--- word_vec_fn.py
+++ word_vec_fn.py
@@ -1,14 +1,39 @@
 import os
+import copy
 
 import numpy as np
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import json
 from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
 from sklearn.datasets import make_multilabel_classification
 from sklearn.model_selection import train_test_split
 
-
 from libact.base.dataset import Dataset
+from libact.labelers import IdealLabeler
+from libact.query_strategies import RandomSampling
+from libact.query_strategies.multilabel import MultilabelWithAuxiliaryLearner
+from libact.query_strategies.multilabel import MMC, BinaryMinimization
+from libact.models.multilabel import BinaryRelevance
+from libact.models import LogisticRegression, SVM
+
+
+def remove_stop_words(sentence):
+    """ Removes stopwords from full_sentences.
+    Parameters
+    ----------
+    sentence: list
+        List of tokenized words.
+    Returns
+    -------
+    filtered_sent : list
+        New sentence with stopwords removed.
+    """
+    stop_words = stopwords.words('english')
+    stop_words.extend((',', '.'))  # Removing punctuation increases accuracy.
+
+    filtered_sent = [w for w in sentence if w.lower() not in stop_words]
+    return filtered_sent
 
 
 def load_glove_model(gloveFile, word_count=100000):
@@ -102,6 +127,12 @@ def sentences2vec(sentences, glove):
     sentence_vectors = []
     for sentence in sentences:
         sentence = word_tokenize(sentence)
+        stop_removed = remove_stop_words(sentence)
+
+        # If, when we remove stopwords, turns of the conversation have no
+        # content, we leave the stopwords in
+        if len(stop_removed )!=0: sentence = stop_removed
+
         matched_words = len(sentence)
         
         # All vectors have the same dimensionality. Using an example
@@ -157,6 +188,13 @@ def full_trn_tst(data_in, test_size, num_labelled):
 
 
 def split_train_test(test_size, num_labelled):
+    """
+    Generates synthetic data for multilabel problem.
+
+    :param test_size:
+    :param num_labelled:
+    :return:
+    """
     # choose a dataset with unbalanced class instances
     # calls random sample generators to build artificial datasets
     # data[0] are the examples; data[1] are the labels
@@ -184,7 +222,7 @@ def split_train_test(test_size, num_labelled):
     return trn_ds, tst_ds, fully_labeled_trn_ds
 
 
-def run(trn_ds, tst_ds, lbr, model, qs, quota, fully_labeled_trn_ds):
+def run(trn_ds, tst_ds, lbr, model, qs, quota, fully_labeled_trn_ds): # Why haven;t we used the fullly_labelled training set? Would be useful to get its loss i.e ideal loss
     C_out, C_out_f1 = [], []
     for _ in range(quota):
         # Query strategy (MMC, BinMin, RandomSampling,
@@ -210,3 +248,106 @@ def run(trn_ds, tst_ds, lbr, model, qs, quota, fully_labeled_trn_ds):
         C_out_f1 = C_out
 
     return C_out, C_out_f1
+
+
+def experiments(fully_labeled_trn_ds, trn_ds, tst_ds, quota):
+    """
+    Runs the active learning experiment for each of the 6 query
+    strategies/criteria combinations.
+
+    :param fully_labeled_trn_ds: libact dataset
+        Fully labelled training examples
+    :param trn_ds: libact dataset
+        Training examples with missing labels
+    :param tst_ds: libact dataset
+        Test dataset
+    :param quota: int
+        number of queries
+    :return: results: dict
+        Key describes the type of experiment, while the value is the
+        corresponding result from the loss function. The value is a list which
+        tracks the Hamming loss and f1 score, in positions 0 and 1. The Hamming
+        loss and f1 score themselves are lists corresponding to the loss after
+        making different amounts of queries.
+    """
+    # lbr simulates an oracle (method label(x) returns x's label)
+    lbr = IdealLabeler(fully_labeled_trn_ds)
+
+    # This is the model which we train and use for predictions. It divides the
+    # task into multiple binary tasks, training one classifier for each label.
+    # Unlike OvA and OvR BinaryRelevance does not use one classifier for each
+    # possible value for the label
+    model = BinaryRelevance(LogisticRegression())
+
+    trn_ds2 = copy.deepcopy(trn_ds)
+    trn_ds3 = copy.deepcopy(trn_ds)
+    trn_ds4 = copy.deepcopy(trn_ds)
+    trn_ds5 = copy.deepcopy(trn_ds)
+    trn_ds6 = copy.deepcopy(trn_ds)
+
+    results = {}
+
+    print "before qs1"
+    # MMC requires a 'base learner' for its binary relevance
+    qs = MMC(trn_ds, br_base=LogisticRegression())
+    E_out_1, E_out_f1_1 = run(trn_ds, tst_ds, lbr, model, qs, quota,
+                              fully_labeled_trn_ds)
+    results['MMC'] = [E_out_1, E_out_f1_1]
+
+    print "before qs2"
+
+    qs2 = RandomSampling(trn_ds2)
+    E_out_2, E_out_f1_2 = run(trn_ds2, tst_ds, lbr, model, qs2, quota,
+                              fully_labeled_trn_ds)
+    results['Random'] = [E_out_2, E_out_f1_2]
+
+    print "before qs3"
+
+    # MLALAL requires two multilabel models:
+    # 1) major_learner:
+    #    Gives a binary output on each label and is used for final prediction
+    # 2) auxiliary_learner:
+    #    Estimates the confident on each label, and gives a real value output
+    #   (supports pred_real method)
+
+    qs3 = MultilabelWithAuxiliaryLearner(
+        trn_ds3,
+        BinaryRelevance(LogisticRegression()),
+        BinaryRelevance(SVM()),
+        criterion='hlr')
+    E_out_3, E_out_f1_3 = run(trn_ds3, tst_ds, lbr, model, qs3, quota,
+                              fully_labeled_trn_ds)
+    results['Aux_hlr'] = [E_out_3, E_out_f1_3]
+
+    print "before qs4"
+
+    qs4 = MultilabelWithAuxiliaryLearner(
+        trn_ds4,
+        BinaryRelevance(LogisticRegression()),
+        BinaryRelevance(SVM()),
+        criterion='shlr')
+    E_out_4, E_out_f1_4 = run(trn_ds4, tst_ds, lbr, model, qs4, quota,
+                              fully_labeled_trn_ds)
+    results['Aux_shlr'] = [E_out_4, E_out_f1_4]
+
+    print "before qs5"
+
+    qs5 = MultilabelWithAuxiliaryLearner(
+        trn_ds5,
+        BinaryRelevance(LogisticRegression()),
+        BinaryRelevance(SVM()),
+        criterion='mmr')
+    E_out_5, E_out_f1_5 = run(trn_ds5, tst_ds, lbr, model, qs5, quota,
+                              fully_labeled_trn_ds)
+    results['Aux_mmr'] = [E_out_5, E_out_f1_5]
+
+    print "before qs6"
+    # BinMin only needs a Continuous Model to eveluate uncertainty
+    qs6 = BinaryMinimization(trn_ds6, LogisticRegression())
+    E_out_6, E_out_f1_6 = run(trn_ds6, tst_ds, lbr, model, qs6, quota,
+                              fully_labeled_trn_ds)
+    results['BinMin'] = [E_out_6, E_out_f1_6]
+
+    print "Done qs6"
+
+    return results
